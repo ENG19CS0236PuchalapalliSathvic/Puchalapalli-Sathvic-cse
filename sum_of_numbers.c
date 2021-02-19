add2de85b81a518bdf35d4703a00b76767ce26a1
ENG19CS0236 Puchalapalli Sathvic <eng19cs0236.puchalapallisathvic@gmail.com>
2021-02-19 18:07:19 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,2 +1,35 @@
 //Write a program to find the sum of n different numbers using 4 functions
 
+#include<stdio.h>
+int input()
+{
+int num;
+printf("enter the number\n");
+scanf("%d",&num);
+return num;
+}
+int compute(int n,int num[n] )
+{
+int i;
+int sum = 0;
+for(int i=0;i<n;i++)
+{
+printf("enter %d numbers\n",i);
+scanf("%d",&num[i]);
+sum = sum + num[i];
+}
+return sum;
+}
+void output(int sum)
+{
+printf("The sum is %d",sum);
+}
+int main()
+{
+int n,sum;
+n = input();
+int num[n];
+sum = compute(n, num);
+output(sum);
+return 0;
+}
