fae47d420f94d215c99a946d03cc487613b55069
ENG19CS0236 Puchalapalli Sathvic <eng19cs0236.puchalapallisathvic@gmail.com>
2021-02-20 21:06:54 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,50 +2,59 @@
 #include<stdio.h>
 typedef struct fraction
 {
-int numerator, denominator;
+  int numerator, denominator;
 }
 Fraction;
-Fraction input(Fractions temp)
+Fraction
+input (Fraction temp)
 {
-printf("Enter the numerator:");
-scanf("%d",&temp.numerator);
-printf("Enter the denominator:");
-scanf("%d",&temp.denominator);
-return temp;
+  printf ("Enter the numerator:");
+  scanf ("%d", &temp.numerator);
+  printf ("Enter the denominator:");
+  scanf ("%d", &temp.denominator);
+  return temp;
 };
-void show_output(Fraction temp,Fraction a, Fraction b)
-{
-printf("Sum of two fraction is %d/%d+%d/%d = %d/%d",a.numerator,a.denominator,b.numerator,b.denominator,temp.numerator,temp.denominator);
-};
-int gcd(int x,int y)
-{
-    if (x ==0)
-    {
-        return y;
-    }
-    return gcd(y%x,x);
-};
-Fractions calc_output(Fraction a, Fraction b)
-{
-    Fractions c;
-    int x,y;
-    x = (a.numerator * b.denominator) + (b.numerator * a.denominator);
-    y = (a.denominator * b.denominator);
 
-    
-    c.numerator = x /gcd(x,y);
-    c.denominator = y /gcd(x,y);
-    return c;
-};
-int main()
+void
+show_output (Fraction temp, Fraction a, Fraction b)
 {
-Fraction a,b,c;
-int gcd;
-printf("Enter 1st fraction:\n");
-a = input(a);
-printf("Enter 2nd fraction:\n");
-b = input(b);
-c = calc_output(a,b);
-show_output(c,a,b);
-return 0;
+  printf ("Sum of two fraction is %d/%d+%d/%d = %d/%d", a.numerator,
+	  a.denominator, b.numerator, b.denominator, temp.numerator,
+	  temp.denominator);
+};
+
+int
+gcd (int x, int y)
+{
+  if (x == 0)
+    {
+      return y;
+    }
+  return gcd (y % x, x);
+};
+
+Fractions
+calc_output (Fraction a, Fraction b)
+{
+  Fractions c;
+  int x, y;
+  x = (a.numerator * b.denominator) + (b.numerator * a.denominator);
+  y = (a.denominator * b.denominator);
+  c.numerator = x / gcd (x, y);
+  c.denominator = y / gcd (x, y);
+  return c;
+};
+
+int
+main ()
+{
+  Fractions a, b, c;
+  int gcd;
+  printf ("Enter 1st fraction:\n");
+  a = input (a);
+  printf ("Enter 2nd fraction:\n");
+  b = input (b);
+  c = calc_output (a, b);
+  show_output (c, a, b);
+  return 0;
 }
