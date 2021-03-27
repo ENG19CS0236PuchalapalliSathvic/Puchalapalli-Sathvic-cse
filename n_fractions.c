9000827b50bc2dcfced9764fa7f95cda419f72ae
ENG19CS0236 Puchalapalli Sathvic <eng19cs0236.puchalapallisathvic@gmail.com>
2021-03-27 14:54:40 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,50 @@
 //WAP to find the sum of n fractions.
+#include <stdio.h>
+struct fraction
+{
+  int n;
+  int d;
+};
+typedef struct fraction Fraction;
+
+Fraction
+getdata (int i)
+{
+  Fraction FractionInput;
+  printf ("Enter the Number %d numerator value: ", i);
+  scanf ("%d", &FractionInput.n);
+  printf ("Enter the Number %d denominator value: ", i);
+  scanf ("%d", &FractionInput.d);
+  return FractionInput;
+}
+
+void
+getN (int n, Fraction s[n])
+{
+  for (int i = 0; i < n; i++)
+    {
+      s[i] = getdata (i+1);
+    }
+}
+
+int
+gcd (int n, int d)
+{
+  if (d != 0)
+    return gcd (d, n % d);
+  else
+    return n;
+}
+
+Fraction
+computeOne (Fraction s1, Fraction s2)
+{
+  Fraction Computevalue;
+  int gcdvalue;
+  Computevalue.n = (s1.n * s2.d) + (s1.d * s2.n);
+  Computevalue.d = (s1.d * s2.d);
+  gcdvalue = gcd (Computevalue.n, Computevalue.d);
+  Computevalue.n = (Computevalue.n / gcdvalue);
+  Computevalue.d = (Computevalue.d / gcdvalue);
+  return Computevalue;
+}
